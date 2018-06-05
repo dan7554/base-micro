ce39fd6e4c034cdd67f3ca62c9961ec6190a45d7
dan <dan7554@gmail>
2018-06-05 14:25:56 -0600

    Added swagger from json via swagger-ui-express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-dist';
 import initRoutes from './routes/index.js';
-import serverConfig from './config/server.json';
+import serverConfig from './config/server';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
+const app = express();
 
 if ( process.argv.includes('--prod') ) {
     app.serverConfig = serverConfig['production'];
@@ -9,10 +12,13 @@ if ( process.argv.includes('--prod') ) {
     app.serverConfig = serverConfig['default'];
 }
 
-const app = express();
 initRoutes(app);
 
-app.use(express.static(swaggerUi.absolutePath()));
+var options = {
+    explorer : true
+  };
+   
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.listen(3110);
 
 console.log('app listening ...', app.serverConfig);
